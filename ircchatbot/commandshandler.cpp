438cb160e01787be11fb143672f4eda9b125ff50
hemirt <hemirt@hemirt.com>
2021-10-18 11:23:45 +0200

    add new commands add new text command tags parse irc message

--- ircchatbot/commandshandler.cpp
+++ ircchatbot/commandshandler.cpp
@@ -1,6 +1,16 @@
 #include "commandshandler.hpp"
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
 #include <iostream>
+#include <vector>
+#include <string_view>
+#include <sstream>
+
+#include "echopage.hpp"
 
 constexpr std::string_view commands_db_name = "commands.db";
 
@@ -12,7 +22,7 @@ CommandsHandler::CommandsHandler()
 
 void CommandsHandler::init_db()
 {
-    auto result = commands_db.execute_statement("CREATE TABLE IF NOT EXISTS commands (trigger TEXT NOT NULL PRIMARY KEY, response TEXT NOT NULL);");
+    auto result = commands_db.execute_statement("CREATE TABLE IF NOT EXISTS commands (trigger TEXT NOT NULL PRIMARY KEY, response TEXT NOT NULL, channels TEXT, users TEXT);");
     if (result.rc != SQLITE_OK)
     {
         std::string msg = "Commands DB table commands error: " + result.errmsg;
@@ -31,7 +41,11 @@ void CommandsHandler::init_db()
 
         for (auto&& line : result.data)
         {
-            textcommands.emplace(*line[0], *line[1]);
+            auto cmd = create_command(line);
+            if (cmd)
+            {
+                commands.emplace(*line[0], std::move(*cmd));
+            }
         }
     }
 
@@ -63,6 +77,75 @@ void CommandsHandler::init_db()
             banphrases.emplace(r_phrase, timeout);
         }
     }
+}
+
+void load_channels(const std::string& channels_string, CommandDetail& cmd)
+{
+    if (channels_string.empty())
+    {
+        return;
+    }
+    
+    switch (channels_string.front())
+    {
+        case '-':
+            cmd.c_include = false;
+            break;
+        case '+':
+        default:
+            cmd.c_include = true;
+            break;
+    }
+    
+    boost::algorithm::split(cmd.channels, channels_string.substr(1), boost::algorithm::is_any_of(","), boost::algorithm::token_compress_on);
+}
+
+void load_users(const std::string& users_string, CommandDetail& cmd)
+{
+    if (users_string.empty())
+    {
+        return;
+    }
+    
+    switch (users_string.front())
+    {
+        case '-':
+            cmd.u_include = false;
+            break;
+        case '+':
+        default:
+            cmd.u_include = true;
+            break;
+    }
+    
+    boost::algorithm::split(cmd.userids, users_string.substr(1), boost::algorithm::is_any_of(","), boost::algorithm::token_compress_on);
+}
+
+constexpr std::size_t COMMAND_DATA_SIZE = 4; // number of db columns
+
+std::optional<CommandDetail> CommandsHandler::create_command(std::vector<std::optional<std::string>> command_data)
+{
+    if (command_data.size() != COMMAND_DATA_SIZE)
+    {
+        return std::nullopt;
+    }
+    
+    CommandDetail cmd;
+    cmd.trigger = *command_data[0];
+    cmd.response = *command_data[1];
+    
+    auto&& channels = command_data[2];
+    if (channels)
+    {
+        load_channels(*channels, cmd);
+    }
+    auto&& users = command_data[3];
+    if (users)
+    {
+        load_users(*users, cmd);
+    }
+    
+    return cmd;
 }
 
 bool CommandsHandler::add_banphrase(std::string_view phrase, int timeout)
@@ -99,7 +182,11 @@ bool CommandsHandler::add_textcommand(std::string_view trigger, std::string_view
 {
     auto result = commands_db.execute_prepared_statement("INSERT INTO commands (trigger, response) VALUES (?, ?);", { std::string(trigger), std::string(response) });
 
-    textcommands.emplace(trigger, response);
+    CommandDetail cmd;
+    cmd.trigger = trigger;
+    cmd.response = response;
+    
+    commands.emplace(trigger, cmd);
 
     return result.rc == SQLITE_OK;
 }
@@ -109,9 +196,9 @@ bool CommandsHandler::remove_textcommand(std::string_view trigger)
 
     auto result = commands_db.execute_prepared_statement("DELETE FROM commands WHERE trigger=?;", { std::string(trigger) });
 
-    if (auto it = textcommands.find(trigger); it != textcommands.end())
+    if (auto it = commands.find(trigger); it != commands.end())
     {
-        textcommands.erase(it);
+        commands.erase(it);
     }
 
     return result.rc == SQLITE_OK;
@@ -135,9 +222,77 @@ std::optional<std::string> CommandsHandler::handle_privmsg(const IrcMessage& irc
         trigger = ircmessage.message.substr(0, space_pos);
     }
 
-    if (auto it = textcommands.find(trigger); it != textcommands.end())
-    {
-        return it->second;
+    if (auto it = commands.find(trigger); it != commands.end())
+    {   
+        auto&& cmd = it->second;
+        
+        if (!cmd.channels.empty())
+        {
+            if (cmd.channels.contains(ircmessage.channel) != cmd.c_include)
+            {
+                return std::nullopt;
+            }
+        }
+        
+        if (!cmd.userids.empty())
+        {
+            if (cmd.userids.contains(ircmessage.user_id) != cmd.u_include)
+            {
+                return std::nullopt;
+            }
+        }
+        
+        auto response_string = cmd.response;
+        
+        std::vector<std::string_view> tokens;
+        boost::algorithm::split(tokens, ircmessage.message, boost::algorithm::is_any_of(" "), boost::algorithm::token_compress_on);
+                
+        {
+            boost::regex e("\\${\\d+}");
+            
+            std::vector<int> vec_num_params{0};  // vector of ${number} numbers
+            boost::match_results<std::string::const_iterator> results;
+            if (boost::regex_search(response_string, results, e)) {
+                for (const auto& result : results) {
+                    vec_num_params.push_back(boost::lexical_cast<int>(std::string_view(result.begin() + 2, result.end() - 1)));
+                }
+            }
+            
+            int maximal_number = *std::max_element(vec_num_params.begin(), vec_num_params.end());
+                        
+            if (maximal_number > tokens.size())
+            {
+                return std::nullopt;
+            }
+            else
+            {
+                std::stringstream ss;
+                
+                for (int i = 0; i <= maximal_number; ++i)
+                {
+                    ss.str(std::string());
+                    ss.clear();
+                    ss << "${" << i << "}";
+                    
+                    boost::algorithm::replace_all(response_string, ss.str(), tokens[i]);
+                }
+            }
+        }
+        
+        {
+            auto& Echo = Hemirt::Utility::EchoPage::get_instance();
+            
+            boost::regex e("\\$url{([^}]+)}");
+            boost::match_results<std::string::const_iterator> results;
+            
+            while (boost::regex_search(response_string, results, e)) {
+                std::string page = results[1];
+                std::string replace = Echo.echo_page(page);
+                response_string = boost::regex_replace(response_string, e, replace, boost::match_default | boost::format_first_only);
+            }
+        }
+        
+        return response_string;
     }
 
     return std::nullopt;
@@ -166,4 +321,104 @@ int CommandsHandler::is_banphrased(std::string_view line)
     }
 
     return total_timeout;
+}
+
+std::string CommandDetail::channels_to_string() const
+{
+    std::string ret = c_include ? "+" : "-";
+    for (auto&& i : channels)
+    {
+        ret += i + ",";
+    }
+    return ret;
+}
+
+std::string CommandDetail::userids_to_string() const
+{
+    std::string ret = u_include ? "+" : "-";
+    for (auto&& i : userids)
+    {
+        ret += i + ",";
+    }
+    return ret;
+}
+
+bool CommandsHandler::add_channel_to_command(std::string_view trigger, std::string_view channel_sv)
+{
+    auto it = commands.find(trigger);
+    if (it == commands.end())
+    {
+        return false;
+    }
+    auto&& cmd = it->second;
+    
+    std::string channel = std::string(channel_sv);
+    boost::algorithm::to_lower(channel);
+    cmd.channels.insert(channel);
+    
+    auto str = cmd.channels_to_string();
+    auto result = commands_db.execute_prepared_statement("UPDATE commands SET channels = ? WHERE trigger = ?;", { std::move(str), std::string(trigger) });
+
+    return result.rc == SQLITE_OK;
+}
+
+bool CommandsHandler::add_userid_to_command(std::string_view trigger, std::string_view userid)
+{
+    auto it = commands.find(trigger);
+    if (it == commands.end())
+    {
+        return false;
+    }
+    auto&& cmd = it->second;
+    
+    cmd.userids.insert(std::string(userid));
+    
+    auto str = cmd.userids_to_string();
+    auto result = commands_db.execute_prepared_statement("UPDATE commands SET users = ? WHERE trigger = ?;", { std::move(str), std::string(trigger) });
+
+    return result.rc == SQLITE_OK;
+}
+
+int CommandsHandler::toggle_channels_to_command(std::string_view trigger)
+{
+    auto it = commands.find(trigger);
+    if (it == commands.end())
+    {
+        return -1;
+    }
+    auto&& cmd = it->second;
+    
+    cmd.c_include = !cmd.c_include;
+    auto str = cmd.channels_to_string();
+    auto result = commands_db.execute_prepared_statement("UPDATE commands SET channels = ? WHERE trigger = ?;", { std::move(str), std::string(trigger) });
+    
+    if (result.rc != SQLITE_OK)
+    {
+        cmd.c_include = !cmd.c_include;
+        return -2;
+    }
+    
+    return cmd.c_include;
+}
+
+int CommandsHandler::toggle_userids_to_command(std::string_view trigger)
+{
+    auto it = commands.find(trigger);
+    if (it == commands.end())
+    {
+        return -1;
+    }
+    auto&& cmd = it->second;
+    
+    cmd.u_include = !cmd.u_include;
+    auto str = cmd.userids_to_string();
+    auto result = commands_db.execute_prepared_statement("UPDATE commands SET users = ? WHERE trigger = ?;", { std::move(str), std::string(trigger) });
+    
+    if (result.rc != SQLITE_OK)
+    {
+        cmd.u_include = !cmd.u_include;
+        return -2;
+    }
+    
+    return cmd.u_include;
 }
--- ircchatbot/commandshandler.hpp
+++ ircchatbot/commandshandler.hpp
@@ -8,8 +8,25 @@
 #include <string>
 #include <string_view>
 #include <boost/algorithm/string/regex.hpp>
+#include <set>
 
 #include "ircmessage.hpp"
+
+using ChannelName = std::string;
+using UserId = std::string;
+using Trigger = std::string;
+
+struct CommandDetail
+{
+    Trigger trigger;
+    std::string response;
+    std::set<ChannelName, std::less<>> channels;
+    bool c_include = true;
+    std::string channels_to_string() const;
+    std::set<UserId, std::less<>> userids;
+    bool u_include = true;
+    std::string userids_to_string() const;
+};
 
 class CommandsHandler
 {
@@ -25,11 +42,19 @@ public:
     /* handle PRIVMSG IrcMessages */
     std::optional<std::string> handle_privmsg(const IrcMessage& ircmessage);
     int is_banphrased(std::string_view line);
+    
+    std::optional<CommandDetail> create_command(std::vector<std::optional<std::string>> command_data);
+
+    bool add_channel_to_command(std::string_view trigger, std::string_view channel);
+    bool add_userid_to_command(std::string_view trigger, std::string_view userid);
+
+    int toggle_userids_to_command(std::string_view trigger);
+    int toggle_channels_to_command(std::string_view trigger);
 
 private:
     Database commands_db;
     std::map<boost::regex, int> banphrases;
-    std::map<std::string, std::string, std::less<>> textcommands;
+    std::map<Trigger, CommandDetail, std::less<>> commands;
 
     void init_db();
 };
